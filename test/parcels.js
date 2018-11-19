630fda9c556c1d2c33f8e7a4e9cb8815d59eb4ca
Hello Tunmbi <hellotunmbi@gmail.com>
2018-11-19 09:06:58 +0100

    added complete url in test

--- test/parcels.js
+++ test/parcels.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const server = require('../server');
 
 const should = chai.should();
+const baseURL = 'https://sendit-hook.herokuapp.com/api/v1';
 
 chai.use(chaiHttp);
 
@@ -10,8 +11,9 @@ describe('Parcels', () => {
 
 	describe('/GET all parcels', () => {
 		it('should Get list of all parcels', (done) => {
-			chai.request(server)
-				.get('/api/v1/parcels')
+			chai
+				.request(server)
+				.get(`${baseURL}/parcels`)
 				.end((err, res) => {
 					res.should.have.status(200);
 					res.should.be.an('object');
